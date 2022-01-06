ffd66fdef15769dbdfd6e6b04d27e8eb9e840019
xhyrom <generalkubo@gmail.com>
2022-01-06 19:01:18 +0100

    omg

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import 'dotenv';
 import { GClient } from 'gcommands';
 import { join } from 'path';
+import { CollectionProvider } from './providers/CollectionProvider';
 
 const client = new GClient({
     intents: [ 'GUILDS', 'GUILD_MESSAGES' ],
     messagePrefix: '!',
     devGuildId: process.env.devGuildId,
+    database: new CollectionProvider(),
     dirs: [
         join(__dirname, 'commands'),
         join(__dirname, 'listeners')
     ]
 })
+
+console.log(client.getDatabase(CollectionProvider.prototype).get('test'));
 
 client.login(process.env.token)
